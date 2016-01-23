7b476ebd3514f0c86ac93bdf5b54b90281aabaa2
Paweł Pela <paulpela@gmail.com>
2016-01-23 18:29:16 +0100

    tried to use bpp value

--- graphics/clear_screen.asm
+++ graphics/clear_screen.asm
@@ -13,13 +13,18 @@ clear_screen:
 
     mov rcx, rax
 
+;    xor rdx, rdx
+;    mov dl, byte [VBEModeInfoBlock.BitsPerPixel] ; not working for some reason
+;    shr dl, 8
+    mov rdx, 3 ; had to hardcode then...
+
     pop rax
     mov rbx, rax
     shr rbx, 8
 .clear:
     mov byte [rdi], al
     mov word [rdi+1], bx
-    add rdi, 3
+    add rdi, rdx
     loop .clear
 
     ret
