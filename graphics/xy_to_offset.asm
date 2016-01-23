51dff3cff17a163c0be12fe604e94dc871fd605d
Paweł Pela <paulpela@gmail.com>
2016-01-23 23:51:28 +0100

    fixed it to use bpp value from VBE mode info block

--- graphics/xy_to_offset.asm
+++ graphics/xy_to_offset.asm
@@ -4,7 +4,14 @@ xy_to_offset:
     mov bx, word [VBEModeInfoBlock.XResolution]
     mul rbx
     add rax, rdi
+    push rax
 
-    mov rbx, 3 ; TODO: remove hardcoded value
+    xor rax, rax
+    mov al, byte [VBEModeInfoBlock.BitsPerPixel]
+    mov rbx, 8
+    div rbx
+    mov rbx, rax
+
+    pop rax
     mul rbx
     ret ; rax holds the offset
