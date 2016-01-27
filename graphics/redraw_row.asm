19e3d10509c6d9a6ac9076cec26e9abafc687f9a
Paweł Pela <paulpela@gmail.com>
2016-01-27 13:08:22 +0100

    added redraw_row

---
+++ graphics/redraw_row.asm
@@ -0,0 +1,49 @@
+USE64
+
+%define ASMLIB_REDRAW_ROW
+
+%ifndef ASMLIB_GET_BYTES_PER_PIXEL
+    %include "asmlib/graphics/get_bytes_per_pixel.asm"
+%endif
+
+; rdi - buffer address
+; rsi - y row
+redraw_row:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+
+    mov rbx, rsi
+    xor rax, rax
+    mov ax, word [VBEModeInfoBlock.XResolution]
+    mov rcx, rax ; save for later
+    mul rbx
+    mov rbx, rax
+
+    call get_bytes_per_pixel
+    mul rbx ; rax now holds the offset
+
+    mov rsi, rdi
+    add rsi, rax
+
+    xor rdi, rdi
+    mov edi, dword [VBEModeInfoBlock.PhysBasePtr]
+    add rdi, rax
+
+    xor rax, rax
+    shr rcx, 3
+.copy:
+    lodsq
+    stosq
+    loop .copy
+
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    ret
