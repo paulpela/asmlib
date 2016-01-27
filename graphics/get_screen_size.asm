02707592db511e3a2faccf02fe129c7af31157e1
Paweł Pela <paulpela@gmail.com>
2016-01-27 12:14:31 +0100

    added pushes and pops

--- graphics/get_screen_size.asm
+++ graphics/get_screen_size.asm
@@ -1,9 +1,13 @@
 %define ASMLIB_GET_SCREEN_SIZE
 
 get_screen_size:
+    push rbx
+
     xor rax, rax
     xor rbx, rbx
     mov ax, word [VBEModeInfoBlock.XResolution]
     mov bx, word [VBEModeInfoBlock.YResolution]
     mul rbx
+
+    pop rbx
     ret
