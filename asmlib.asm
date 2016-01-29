1812bc5315970bde1e4ee72c4a73c21a314a6e3e
Paweł Pela <paulpela@gmail.com>
2016-01-29 14:47:05 +0100

    pure64 is on always

--- asmlib.asm
+++ asmlib.asm
@@ -1,12 +1,9 @@
 %define ASMLIB
 
+%include "asmlib/pure64/pure64.asm"
 %include "asmlib/system.asm"
 %include "asmlib/math.asm"
 %include "asmlib/graphics.asm"
-
-%ifdef USE_PURE64
-    %include "asmlib/pure64/pure64.asm"
-%endif
 
 %ifdef DEBUG
     %include "asmlib/nasm/debug.asm"
