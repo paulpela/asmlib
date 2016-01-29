abfffa746da7003ea164a6f7226296684da55e3d
Paweł Pela <paulpela@gmail.com>
2016-01-29 13:58:18 +0100

    added conditional

--- graphics/macros.asm
+++ graphics/macros.asm
@@ -1,10 +1,12 @@
-%macro copy_point 2
-    push rax 
+%ifnmacro copy_point
+    %macro copy_point 2
+        push rax 
 
-    mov ax, word [%1+point.x]
-    mov word [%2+point.x], ax
-    mov ax, [%1+point.y]
-    mov word [%2+point.y], ax
+        mov ax, word [%1+point.x]
+        mov word [%2+point.x], ax
+        mov ax, [%1+point.y]
+        mov word [%2+point.y], ax
 
-    pop rax
-%endmacro
+        pop rax
+    %endmacro
+%endif
