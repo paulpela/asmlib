8add09572149f3c4a37b6ce6a948641b99cc2f2c
Pawel Pela <paulpela@gmail.com>
2016-02-01 22:23:08 +0100

    reduced number of calls

--- graphics/restore_bg_buffer.asm
+++ graphics/restore_bg_buffer.asm
@@ -2,32 +2,16 @@ USE64
 
 %define ASMLIB_RESTORE_BG_BUFFER
 
-%ifndef ASMLIB_GET_SCREEN_SIZE
-    %include "asmlib/graphics/get_screen_size.asm"
-%endif
-
-%ifndef ASMLIB_GET_BYTES_PER_PIXEL
-    %include "asmlib/graphics/get_bytes_per_pixel.asm"
-%endif
-
 ; rdi - buffer address
 restore_bg_buffer:
     push rax
-    push rbx
     push rcx
-    push rdx
     push rsi
     push rdi
 
     mov rsi, rdi
 
-    call get_screen_size
-    mov rbx, rax
-
-    call get_bytes_per_pixel
-    mul rbx ; rax now holds buffer size in bytes
-
-    mov rcx, rax
+    mov rcx, SCREEN_RES_X * SCREEN_RES_Y * BYTES_PER_PIXEL
     shr rcx, 3
     xor rax, rax
     xor rdi, rdi
@@ -39,8 +23,6 @@ restore_bg_buffer:
 
     pop rdi
     pop rsi
-    pop rdx
     pop rcx
-    pop rbx
     pop rax
     ret
